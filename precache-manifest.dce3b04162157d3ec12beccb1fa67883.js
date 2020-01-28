25dcc4af2fa03bd727045d6159ed452a116e940c
akshaybadodiya <akshayindore@gmail.com>
2020-01-28 09:48:13 +0530

    Updates

---
+++ precache-manifest.dce3b04162157d3ec12beccb1fa67883.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7356e36201c3ee79493f51cdea3c2380",
+    "url": "/qr-reader/index.html"
+  },
+  {
+    "revision": "1c2a26fe90ae150e4221",
+    "url": "/qr-reader/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a8cb36f4134294d6c8d2",
+    "url": "/qr-reader/static/js/2.6a7a67db.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/qr-reader/static/js/2.6a7a67db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c2a26fe90ae150e4221",
+    "url": "/qr-reader/static/js/main.c825f194.chunk.js"
+  },
+  {
+    "revision": "12621912f0242969253c",
+    "url": "/qr-reader/static/js/runtime-main.b3ae3826.js"
+  }
+]);
